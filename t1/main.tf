f1ddd8969cfe92076df40b98996672aeeec5cb02
Dmitry Boltrushko <bdimonik@gmail.com>
2022-03-10 15:28:10 +0300

    sample template

---
+++ t1/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "test_instance" {
+    ami             = "ami-0e1d30f2c40c4c701"
+    instance_type   = "t2.micro"
+    subnet_id       = "subnet-5c541774" 
+}
